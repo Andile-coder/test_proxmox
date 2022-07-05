13a37155423f308476d1fd5ca71a4a4978337c8a
Andile-coder <maselaandile@gmail.com>
2022-07-06 01:18:23 +0200

    init

---
+++ cleandata.js
@@ -0,0 +1,46 @@
+const vms = require("./vms.json");
+
+console.log(vms);
+//how many networks each vm
+
+const getNets = () => {
+  let networkCount = [];
+  let nets = [];
+
+  vms.forEach((vm) => {
+    let count = 0;
+    let tempNets = [];
+    for (const [key, value] of Object.entries(vm)) {
+      if (key.includes("net")) {
+        count++;
+        let net1 = value.split(",").map((elem) => elem.split("="));
+        let net2 = {};
+        net1.forEach((elem) => {
+          switch (elem[0]) {
+            case "virtio":
+              net2 = { ...net2, virtio: elem[1] };
+              break;
+            case "tag":
+              net2 = { ...net2, tag: elem[1] };
+              break;
+            case "bridge":
+              net2 = { ...net2, bridge: elem[1] };
+              break;
+            case "e1000":
+              net2 = { ...net2, e1000: elem[1] };
+              break;
+          }
+        });
+
+        tempNets.push(net2);
+      }
+    }
+
+    networkCount.push(count);
+    nets.push(tempNets);
+  });
+
+  const newVms = vms.map((vm, index) => {
+    return { ...vm, networks: nets[index] };
+  });
+};
--- index.js
+++ index.js
@@ -1,37 +1,108 @@
-const process = require("dotenv");
+const process = require("dotenv").config();
 const axios = require("axios");
+const fs = require("fs");
 const pve = require("@corsinvest/cv4pve-api-javascript");
 const Test = async () => {
-  var client = new pve.PveClient("141.94.141.230", 8006);
-  var login = await client.login("root", "8ZAV7cvMqlH8TS1S", "pam");
+  var client = new pve.PveClient("mwprox04.cloudoffice.co.za", 8006);
+  var login = await client.login(
+    "noc_audit",
+    "Friday-Uproar6-Wrongdoer-Issuing",
+    "pve"
+  );
   if (login) {
     console.log("login", login);
+    const configuredQemu = [];
+    const configuredLxc = [];
+    const configuredPools = [];
     //get nodes
     const nodes = (await client.nodes.index()).response;
-    const configured = [];
+
+    //get all pools
+    const getPools = async () => {
+      const pools = (await client.pools.index()).response;
+      for (let i = 0; i < pools.data?.length; i++) {
+        let pool = (await client.pools.get(pools.data[i].poolid).readPool())
+          .response;
+        pool = { ...pool.data, poolid: pools.data[i].poolid };
+        configuredPools.push(pool);
+      }
+      return configuredPools;
+    };
     //get qemu of all nodes
     async function getQemu() {
       for (let i = 0; i < nodes?.data?.length; i++) {
-        const qemus = (
-          await client.nodes.get(nodes.data[i].node).qemu.vmlist(0)
-        ).response;
+        const qemu = (await client.nodes.get(nodes.data[i].node).qemu.vmlist(0))
+          .response;
 
-        for (let j = 0; j < qemus.data.length; j++) {
-          const item = (
-            await client.nodes.get("test-prox").qemu.get(103).config.vmConfig()
+        for (let j = 0; j < qemu.data.length; j++) {
+          let item = (
+            await client.nodes
+              .get(nodes.data[i].node)
+              .qemu.get(qemu.data[j].vmid)
+              .config.vmConfig()
           ).response;
-          configured.push(item);
+          item = {
+            ...item.data,
+            status: qemu.data[j]?.status,
+            vmid: qemu.data[j]?.vmid,
+          };
+          configuredQemu.push(item);
         }
       }
-
-      return configured;
+      return configuredQemu;
     }
+    //get lxc of all nodes
+    const getLxc = async () => {
+      for (let i = 0; i < nodes?.data?.length; i++) {
+        const lxc = (await client.nodes.get(nodes.data[i].node).lxc.vmlist())
+          .response;
+        for (let j = 0; j < lxc.data.length; j++) {
+          let item = (
+            await client.nodes
+              .get(nodes.data[i].node)
+              .lxc.get(lxc.data[j].vmid)
+              .config.vmConfig()
+          ).response;
+          item = {
+            ...item.data,
+            status: lxc.data[j]?.status,
+            vmid: lxc.data[j]?.vmid,
+          };
 
-    const secondFoo = async () => {
-      const result = await getQemu();
-      console.log(result);
+          configuredLxc.push(item);
+        }
+      }
+      return configuredLxc;
     };
-    secondFoo();
+
+    //create json file
+
+    const jsonFilecreator = (data, fileName) => {
+      try {
+        const stringifyData = JSON.stringify(data);
+        fs.writeFile(`${fileName}.json`, stringifyData, (err) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log("file has been saved");
+          }
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    const runner = async () => {
+      const qemu = await getQemu();
+      const lxc = await getLxc();
+      const pools = await getPools();
+      jsonFilecreator(qemu, "vms");
+      jsonFilecreator(lxc, "containers");
+      jsonFilecreator(pools, "pools");
+    };
+    runner();
+  } else {
+    console.log("login", login);
   }
 };
 
